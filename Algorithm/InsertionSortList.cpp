f0c98d5dab0d99ace2eb7a2c1c635d21e70ca3d3
ge.feng <gefengsteve@gmail.com>
2017-08-11 00:55:15 +0100

    InsertionSortList

--- Algorithm/InsertionSortList.cpp
+++ Algorithm/InsertionSortList.cpp
@@ -4,36 +4,44 @@
 // O(n^2)
 ListNode* InsertionSortList::solution(ListNode* head)
 {
-	ListNode* currNode = NULL;
+	ListNode* currNode = head;
+	ListNode* prevNode = NULL;
 	ListNode* tempNode = NULL;
-	int temp = 0;
-
-	if (head == NULL)
-		return NULL;
-
-	currNode = head->next;
-
-	if (currNode == NULL)
-		return head;
+	ListNode* newHead = head;
 
 	while (currNode != NULL)
 	{
-		tempNode = head;
+		tempNode = newHead;
+		prevNode = NULL;
 
-		while (tempNode != currNode)
+		while (currNode->next != NULL && tempNode != currNode->next)
 		{
-			if (tempNode->val > currNode->val)
+			if (tempNode->val > currNode->next->val)
 			{
-				temp = currNode->val;
-				currNode->val = tempNode->val;
-				tempNode->val = temp;
+				if (prevNode != NULL)
+				{
+					prevNode->next = currNode->next;
+					currNode->next = currNode->next->next;
+					prevNode->next->next = tempNode;
+					currNode = tempNode;
+				}
+				else
+				{
+					newHead = currNode->next;
+					currNode->next = currNode->next->next;
+					newHead->next = tempNode;
+					currNode = tempNode;
+				}
+
+				break;
 			}
 
+			prevNode = tempNode;
 			tempNode = tempNode->next;
 		}
 
 		currNode = currNode->next;
 	}
 
-	return head;
+	return newHead;
 }
