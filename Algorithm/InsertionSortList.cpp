61036d43e3747243afb9c48296c7f841ea256843
unknown <ge.feng@gefeng.cr2.local>
2017-08-11 15:12:32 +0100

    Work out InsertionSortList

--- Algorithm/InsertionSortList.cpp
+++ Algorithm/InsertionSortList.cpp
@@ -14,6 +14,8 @@ ListNode* InsertionSortList::solution(ListNode* head)
 		tempNode = newHead;
 		prevNode = NULL;
 
+		if (currNode->next != NULL && currNode->val > currNode->next->val)
+
 		while (currNode->next != NULL && tempNode != currNode->next)
 		{
 			if (tempNode->val > currNode->next->val)
@@ -23,14 +25,12 @@ ListNode* InsertionSortList::solution(ListNode* head)
 					prevNode->next = currNode->next;
 					currNode->next = currNode->next->next;
 					prevNode->next->next = tempNode;
-					currNode = tempNode;
 				}
 				else
 				{
 					newHead = currNode->next;
 					currNode->next = currNode->next->next;
 					newHead->next = tempNode;
-					currNode = tempNode;
 				}
 
 				break;
